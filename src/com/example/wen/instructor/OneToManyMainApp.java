e43b9dd7e2beb51f2d9d9d32663b79c1275bcb1b
wen-zhao <wen-zhao@outlook.com>
2017-10-15 08:31:10 +0300

    fix issue of duplicated row

--- src/com/example/wen/instructor/OneToManyMainApp.java
+++ src/com/example/wen/instructor/OneToManyMainApp.java
@@ -10,22 +10,26 @@ import org.hibernate.cfg.Configuration;
 public class OneToManyMainApp {
 
 	public static void main(String[] args) {
-		SessionFactory factory = new Configuration().configure().addAnnotatedClass(Instructor.class).addAnnotatedClass(Course.class).buildSessionFactory();
+		SessionFactory factory = new Configuration().configure().addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class).buildSessionFactory();
 		Session session = factory.getCurrentSession();
 		
-		Instructor instructor = new Instructor("wen","li","wen@gamil.com");
-		InstructorDetail detail = new InstructorDetail("youtube.com","yogo");
+		Instructor instructor = new Instructor("sally","kun","sally@gamil.com");
+		InstructorDetail detail = new InstructorDetail("youtube.com","tv");
 		instructor.setInstructorDetail(detail);
-		Course javaCourse = new Course("java");
-		Course cplus = new Course("c++");
+		Course javaCourse = new Course("java advanced");
+		Course cplus = new Course("c++ advanced");
 		List<Course> courses = new ArrayList<>();
+		javaCourse.setInstructor(instructor);
+		cplus.setInstructor(instructor);
 		courses.add(javaCourse);
 		courses.add(cplus);        
 		instructor.setCourses(courses);
+		
 	    try{
 	    	session.beginTransaction();
 	    	session.save(instructor);
-	    	
+	    	session.save(javaCourse);
+	    	session.save(cplus);
 	    	session.getTransaction().commit();
 		   
 	   }catch (Exception e){
